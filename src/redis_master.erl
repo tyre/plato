d80e60f955ac9fbe3d4b41c201ec9154337afec8
Ezekiel Smithburg <esmithburg@marchex.com>
2012-09-23 00:38:41 -0500

    generate the query string automagically.
    currently untested, because i don't know enough about the project to test it properly yet.  it works in the limited cases i was able to test, though!

--- src/redis_master.erl
+++ src/redis_master.erl
@@ -7,9 +7,9 @@ loop() ->
       spawn(redis_worker, init, []) ! list_to_tuple([self() | Msg]),
       loop();
     {_From, getkey, Params, Callback} ->
-      % ParamString = parameterize(dict:new(), Params, ""),
-      % io:format(ParamString),
-      {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Body}} = httpc:request("http://localhost:7737/generate/event?app-name=Facebook&event-name=pageView&time=8764326"),
+      ParamString = parameterize(Params),
+      io:format(ParamString),
+      {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Body}} = httpc:request(string:concat("http://localhost:7737/generate/event?", ParamString)),
       ParsedJson = jsx:decode(list_to_binary(Body)),
       Key = dict:fetch("key", ParsedJson),
       io:format("Your body: ~n~n~s~n~n", [Key]),
@@ -22,10 +22,4 @@ loop() ->
       true
   end.
 
-parameterize(Dict, Keys, Acc) ->
-  case Keys of
-    [H|_] ->
-      string:concat(Acc, parameterize(Dict, lists:delete(H, Dict), Acc));
-    true ->
-      Acc
-  end.
+parameterize(Keys) -> string:join(lists:map(fun ({Key, Val}) -> string:join([Key, Val], "=") end, dict:to_list(Keys)), "&").
